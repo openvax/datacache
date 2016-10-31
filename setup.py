39e68245cf7fe73f460f68e150421c7f7efbc86c
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2016-10-31 14:18:00 -0400

    version bump for ENV fix

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ except:
 if __name__ == "__main__":
     setup(
         name="datacache",
-        version="0.4.19",
+        version="0.4.20",
         description="Helpers for transparently downloading datasets",
         author="Alex Rubinsteyn",
         author_email="alex {dot} rubinsteyn {at} mssm {dot} edu",
